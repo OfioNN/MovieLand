bd5c9584ebc4ba2b7e30edfcee3826a96f379f6e
Dawid Szymik <dawid_szymik@wp.pl>
2024-12-04 22:16:26 +0100

    Update label width in CSS for improved layout consistency

--- scriptRandom.js
+++ scriptRandom.js
@@ -1,8 +1,8 @@
 const movies = [
         { 
             title: "Inception", 
-            categories: ["Action", "Sci-Fi"], 
-            streamingPlatform: "Netflix", 
+            categories: ["Action", "Adventure", "Sci-Fi", "Thriller"], 
+            streamingPlatform: "HBO Max", 
             pg: "13", 
             duration: "2h 28min" 
         },
@@ -15,22 +15,22 @@ const movies = [
         },
         { 
             title: "The Dark Knight", 
-            categories: ["Action", "Drama"], 
+            categories: ["Action", "Drama", "Crime", "Thriller"], 
             streamingPlatform: "HBO Max", 
             pg: "13", 
             duration: "2h 32min" 
         },
         { 
             title: "Pulp Fiction", 
-            categories: ["Crime", "Comedy"], 
-            streamingPlatform: "Hulu", 
+            categories: ["Crime", "Comedy", "Drama"], 
+            streamingPlatform: "Prime Video", 
             pg: "R", 
             duration: "2h 34min" 
         },
         { 
             title: "The Matrix", 
             categories: ["Sci-Fi", "Action"], 
-            streamingPlatform: "Netflix", 
+            streamingPlatform: "HBO Max", 
             pg: "R", 
             duration: "2h 16min" 
         },
@@ -44,7 +44,7 @@ const movies = [
         { 
             title: "Forrest Gump", 
             categories: ["Drama", "Romance"], 
-            streamingPlatform: "Paramount+", 
+            streamingPlatform: "Prime Video", 
             pg: "13", 
             duration: "2h 22min" 
         },
@@ -57,7 +57,7 @@ const movies = [
         },
         { 
             title: "Interstellar", 
-            categories: ["Sci-Fi", "Drama"], 
+            categories: ["Sci-Fi", "Drama", "Adventure"], 
             streamingPlatform: "HBO Max", 
             pg: "13", 
             duration: "2h 49min" 
@@ -71,14 +71,14 @@ const movies = [
         },
         { 
             title: "The Lion King", 
-            categories: ["Animation", "Drama"], 
+            categories: ["Animation", "Drama", "Adventure", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 28min" 
         },
         { 
             title: "Gladiator", 
-            categories: ["Action", "Drama"], 
+            categories: ["Action", "Drama", "Adventure"], 
             streamingPlatform: "HBO Max", 
             pg: "R", 
             duration: "2h 35min" 
@@ -88,39 +88,39 @@ const movies = [
             categories: ["Romance", "Drama"], 
             streamingPlatform: "Paramount+", 
             pg: "13", 
-            duration: "3h 15min" 
+            duration: "3h 14min" 
         },
         { 
             title: "Jurassic Park", 
-            categories: ["Sci-Fi", "Adventure"], 
-            streamingPlatform: "Peacock", 
+            categories: ["Sci-Fi", "Adventure", "Action", "Thriller"], 
+            streamingPlatform: "Prime Video", 
             pg: "13", 
             duration: "2h 7min" 
         },
         { 
             title: "The Silence of the Lambs", 
-            categories: ["Thriller", "Crime"], 
-            streamingPlatform: "HBO Max", 
+            categories: ["Thriller", "Crime", "Drama"], 
+            streamingPlatform: "Prime Video", 
             pg: "R", 
             duration: "1h 58min" 
         },
         { 
             title: "The Social Network", 
             categories: ["Drama", "Biography"], 
-            streamingPlatform: "Netflix", 
+            streamingPlatform: "Hbo Max", 
             pg: "13", 
-            duration: "2h 0min" 
+            duration: "2h" 
         },
         { 
             title: "The Wolf of Wall Street", 
-            categories: ["Comedy", "Drama"], 
+            categories: ["Comedy", "Drama", "Crime", "Biography"], 
             streamingPlatform: "Prime Video", 
             pg: "R", 
-            duration: "3h 0min" 
+            duration: "3h" 
         },
         { 
             title: "The Grand Budapest Hotel", 
-            categories: ["Comedy", "Drama"], 
+            categories: ["Comedy", "Crime", "Adventure"], 
             streamingPlatform: "Hulu", 
             pg: "R", 
             duration: "1h 39min" 
@@ -128,104 +128,104 @@ const movies = [
         { 
             title: "Whiplash", 
             categories: ["Drama", "Music"], 
-            streamingPlatform: "HBO Max", 
+            streamingPlatform: "Prime Video", 
             pg: "R", 
             duration: "1h 46min" 
         },
         { 
             title: "Parasite", 
-            categories: ["Drama", "Thriller"], 
+            categories: ["Drama", "Thriller", "Comedy"], 
             streamingPlatform: "Hulu", 
             pg: "R", 
             duration: "2h 12min" 
         },
         { 
             title: "Mad Max: Fury Road", 
-            categories: ["Action", "Adventure"], 
+            categories: ["Action", "Adventure", "Sci-Fi"], 
             streamingPlatform: "HBO Max", 
             pg: "R", 
-            duration: "2h 0min" 
+            duration: "2h" 
         },
         { 
             title: "Coco", 
-            categories: ["Animation", "Family"], 
+            categories: ["Animation", "Family", "Adventure", "Music", "Fantasy"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 45min" 
         },
         { 
             title: "Spider-Man: Into the Spider-Verse", 
-            categories: ["Animation", "Action"], 
+            categories: ["Animation", "Action", "Adventure", "Comedy"], 
             streamingPlatform: "Netflix", 
             pg: "13", 
             duration: "1h 56min" 
         },
         { 
             title: "Avengers: Endgame", 
-            categories: ["Action", "Sci-Fi"], 
+            categories: ["Action", "Sci-Fi", "Adventure", "Drama"], 
             streamingPlatform: "Disney+", 
             pg: "13", 
             duration: "3h 1min" 
         },
         { 
             title: "Shrek", 
-            categories: ["Animation", "Comedy"], 
+            categories: ["Animation", "Comedy", "Adventure"], 
             streamingPlatform: "Netflix", 
             pg: "13", 
             duration: "1h 30min" 
         },
         { 
             title: "The Incredibles", 
-            categories: ["Animation", "Action"], 
+            categories: ["Animation", "Action", "Adventure", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 55min" 
         },
         { 
             title: "Frozen", 
-            categories: ["Animation", "Family"], 
+            categories: ["Animation", "Family", "Adventure", "Music", "Comedy"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 42min" 
         },
         { 
             title: "Zootopia", 
-            categories: ["Animation", "Comedy"], 
+            categories: ["Animation", "Comedy", "Adventure", "Family", "Crime"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 48min" 
         },
         { 
             title: "Finding Nemo", 
-            categories: ["Animation", "Adventure"], 
+            categories: ["Animation", "Adventure", "Comedy", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 40min" 
         },
         { 
             title: "Toy Story", 
-            categories: ["Animation", "Comedy"], 
+            categories: ["Animation", "Comedy", "Adventure", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 21min" 
         },
         { 
             title: "The Revenant", 
-            categories: ["Drama", "Adventure"], 
+            categories: ["Drama", "Adventure", "Thriller"], 
             streamingPlatform: "HBO Max", 
             pg: "R", 
             duration: "2h 36min" 
         },
         { 
             title: "1917", 
-            categories: ["War", "Drama"], 
+            categories: ["War", "Drama", "Action"], 
             streamingPlatform: "Hulu", 
             pg: "R", 
             duration: "1h 59min" 
         },
         { 
             title: "Saving Private Ryan", 
-            categories: ["War", "Drama"], 
+            categories: ["War", "Drama", "Action"], 
             streamingPlatform: "Paramount+", 
             pg: "R", 
             duration: "2h 49min" 
@@ -246,105 +246,105 @@ const movies = [
         },
         { 
             title: "Once Upon a Time in Hollywood", 
-            categories: ["Comedy", "Drama"], 
+            categories: ["Comedy", "Drama", "Crime"], 
             streamingPlatform: "Hulu", 
             pg: "R", 
             duration: "2h 41min" 
         },
         { 
             title: "Knives Out", 
-            categories: ["Mystery", "Comedy"], 
+            categories: ["Comedy", "Drama", "Crime", "Thriller"], 
             streamingPlatform: "Prime Video", 
             pg: "13", 
             duration: "2h 10min" 
         },
         { 
             title: "Tenet", 
-            categories: ["Sci-Fi", "Action"], 
+            categories: ["Sci-Fi", "Action", "Thriller"], 
             streamingPlatform: "HBO Max", 
             pg: "13", 
             duration: "2h 30min" 
         },
         { 
             title: "Black Panther", 
-            categories: ["Action", "Adventure"], 
+            categories: ["Action", "Adventure", "Sci-Fi"], 
             streamingPlatform: "Disney+", 
             pg: "13", 
             duration: "2h 14min" 
         },
         { 
             title: "Doctor Strange", 
-            categories: ["Sci-Fi", "Action"], 
+            categories: ["Sci-Fi", "Action", "Adventure", "Fantasy"], 
             streamingPlatform: "Disney+", 
             pg: "13", 
             duration: "1h 55min" 
         },
         { 
             title: "Logan", 
-            categories: ["Action", "Drama"], 
+            categories: ["Action", "Drama", "Sci-Fi", "Thriller"], 
             streamingPlatform: "Hulu", 
             pg: "R", 
             duration: "2h 17min" 
         },
         { 
             title: "Joker", 
-            categories: ["Drama", "Thriller"], 
+            categories: ["Drama", "Thriller", "Crime"], 
             streamingPlatform: "HBO Max", 
             pg: "R", 
             duration: "2h 2min" 
         },
         { 
             title: "La La Land", 
-            categories: ["Romance", "Music"], 
+            categories: ["Romance", "Music", "Drama"], 
             streamingPlatform: "Hulu", 
             pg: "PG-13", 
             duration: "2h 8min" 
         },
         { 
             title: "The Greatest Showman", 
-            categories: ["Drama", "Music"], 
+            categories: ["Drama", "Music", "Biography"], 
             streamingPlatform: "Netflix", 
             pg: "PG", 
             duration: "1h 45min" 
         },        
         {
             title: "A Star is Born", 
-            categories: ["Drama", "Romance"], 
+            categories: ["Drama", "Romance", "Music"], 
             streamingPlatform: "HBO Max", 
             pg: "R", 
             duration: "2h 16min" 
         },
         {
             title: "Moana", 
-            categories: ["Animation", "Adventure"], 
+            categories: ["Animation", "Adventure", "Comedy", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 47min" 
         },
         {
             title: "Soul", 
-            categories: ["Animation", "Music"], 
+            categories: ["Animation", "Music", "Adventure", "Comedy", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 40min" 
         },
         {
             title: "The Secret Life of Walter Mitty", 
-            categories: ["Adventure", "Comedy"], 
+            categories: ["Adventure", "Comedy", "Drama"], 
             streamingPlatform: "Hulu", 
             pg: "PG", 
             duration: "1h 54min" 
         },
         {
             title: "The Truman Show", 
-            categories: ["Drama", "Sci-Fi"], 
+            categories: ["Drama", "Sci-Fi", "Comedy"], 
             streamingPlatform: "Prime Video", 
             pg: "PG", 
             duration: "1h 43min" 
         },
         {
             title: "Up", 
-            categories: ["Animation", "Adventure"], 
+            categories: ["Animation", "Adventure", "Comedy", "Family"], 
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 36min" 
@@ -363,6 +363,7 @@ var romanceCheck = $("#romance");
 var fantasyCheck = $("#fantasy");
 var warCheck = $("#war");
 var scifiCheck = $("#scifi");
+var crimeCheck = $("#crime");
 var biographyCheck = $("#biography");
 var adventureCheck = $("#adventure");
 var familyCheck = $("#family");
@@ -402,6 +403,7 @@ function randomMovie() {
     if (musicCheck.is(":checked")) selectedCategories.push("Music");
     if (dramaCheck.is(":checked")) selectedCategories.push("Drama");
     if (animationCheck.is(":checked")) selectedCategories.push("Animation");
+    if (crimeCheck.is(":checked")) selectedCategories.push("Crime");
 
     const filteredMovies = movies.filter(movie => {
         if(selectedCategories.length > 0) {
