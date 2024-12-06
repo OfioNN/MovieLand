07807799653ea9685c53d21664ea44b5a17cfcbc
Dawid Szymik <dawid_szymik@wp.pl>
2024-12-06 01:17:43 +0100

    Refactor navigation menu structure and improve responsiveness; enhance user experience across devices

--- scriptRandom.js
+++ scriptRandom.js
@@ -348,7 +348,438 @@ const movies = [
             streamingPlatform: "Disney+", 
             pg: "G", 
             duration: "1h 36min" 
+        },
+        {
+            title: "Blade Runner 2049",
+            categories: ["Sci-Fi", "Drama", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 44min"
+        },
+        {
+            title: "The Irishman",
+            categories: ["Crime", "Drama", "Biography"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "3h 29min"
+        },
+        {
+            title: "The Lego Movie",
+            categories: ["Animation", "Comedy", "Adventure", "Family"],
+            streamingPlatform: "HBO Max",
+            pg: "PG",
+            duration: "1h 40min"
+        },
+        {
+            title: "Arrival",
+            categories: ["Sci-Fi", "Drama", "Mystery"],
+            streamingPlatform: "Hulu",
+            pg: "PG-13",
+            duration: "1h 56min"
+        },
+        {
+            title: "Edge of Tomorrow",
+            categories: ["Action", "Sci-Fi", "Adventure"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "1h 53min"
+        },
+        {
+            title: "The Pianist",
+            categories: ["Drama", "Biography", "War"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 30min"
+        },
+        {
+            title: "Inglourious Basterds",
+            categories: ["Action", "Drama", "War"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 33min"
+        },
+        {
+            title: "Shutter Island",
+            categories: ["Thriller", "Drama"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "2h 18min"
+        },
+        {
+            title: "Inside Out",
+            categories: ["Animation", "Family", "Comedy", "Drama"],
+            streamingPlatform: "Disney+",
+            pg: "PG",
+            duration: "1h 35min"
+        },
+        {
+            title: "Se7en",
+            categories: ["Crime", "Drama", "Thriller"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 7min"
+        },
+        {
+            title: "The Prestige",
+            categories: ["Drama", "Sci-Fi", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "2h 10min"
+        },
+        {
+            title: "The Green Mile",
+            categories: ["Drama", "Crime", "Fantasy"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "3h 9min"
+        },
+        {
+            title: "Good Will Hunting",
+            categories: ["Drama", "Romance"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 6min"
+        },
+        {
+            title: "The Departed",
+            categories: ["Crime", "Drama", "Thriller"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "2h 31min"
+        },
+        {
+            title: "In Bruges",
+            categories: ["Comedy", "Drama", "Crime"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "1h 47min"
+        },
+        {
+            title: "No Country for Old Men",
+            categories: ["Crime", "Drama", "Thriller"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "2h 2min"
+        },
+        {
+            title: "The Big Lebowski",
+            categories: ["Comedy", "Crime"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "1h 57min"
+        },
+        {
+            title: "12 Angry Men",
+            categories: ["Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "G",
+            duration: "1h 36min"
+        },
+        {
+            title: "Prisoners",
+            categories: ["Crime", "Drama", "Thriller"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "2h 33min"
+        },
+        {
+            title: "Dunkirk",
+            categories: ["War", "Drama", "Action"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "1h 46min"
+        },
+        {
+            title: "Hacksaw Ridge",
+            categories: ["War", "Drama", "Biography"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 19min"
+        },
+        {
+            title: "Fury",
+            categories: ["War", "Action", "Drama"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "2h 14min"
+        },
+        {
+            title: "Platoon",
+            categories: ["War", "Drama"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "2h"
+        },
+        {
+            title: "Apocalypse Now",
+            categories: ["War", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 27min"
+        },
+        {
+            title: "The Thin Red Line",
+            categories: ["War", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 50min"
+        },
+        {
+            title: "1917",
+            categories: ["War", "Drama", "Action"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "1h 59min"
+        },
+        {
+            title: "Black Hawk Down",
+            categories: ["War", "Action", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 24min"
+        },
+        {
+            title: "Letters from Iwo Jima",
+            categories: ["War", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 21min"
+        },
+        {
+            title: "Enemy at the Gates",
+            categories: ["War", "Drama", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 11min"
+        },
+        {
+            title: "The Bridge on the River Kwai",
+            categories: ["War", "Drama", "Adventure"],
+            streamingPlatform: "Prime Video",
+            pg: "PG",
+            duration: "2h 41min"
+        },
+        {
+            title: "All Quiet on the Western Front",
+            categories: ["War", "Drama"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "2h 28min"
+        },
+        {
+            title: "Bohemian Rhapsody",
+            categories: ["Biography", "Drama", "Music"],
+            streamingPlatform: "Hulu",
+            pg: "PG-13",
+            duration: "2h 14min"
+        },
+        {
+            title: "The Imitation Game",
+            categories: ["Biography", "Drama", "War"],
+            streamingPlatform: "Prime Video",
+            pg: "PG-13",
+            duration: "1h 54min"
+        },
+        {
+            title: "Schindler's List",
+            categories: ["Biography", "Drama", "War"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "3h 15min"
+        },
+        {
+            title: "Steve Jobs",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 2min"
+        },
+        {
+            title: "The Theory of Everything",
+            categories: ["Biography", "Drama", "Romance"],
+            streamingPlatform: "Prime Video",
+            pg: "PG-13",
+            duration: "2h 3min"
+        },
+        {
+            title: "Lincoln",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "2h 30min"
+        },
+        {
+            title: "Rocketman",
+            categories: ["Biography", "Drama", "Music"],
+            streamingPlatform: "Paramount+",
+            pg: "R",
+            duration: "2h 1min"
+        },
+        {
+            title: "Hidden Figures",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "Disney+",
+            pg: "PG",
+            duration: "2h 7min"
+        },
+        {
+            title: "Catch Me If You Can",
+            categories: ["Biography", "Crime", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "PG-13",
+            duration: "2h 21min"
+        },
+        {
+            title: "A Beautiful Mind",
+            categories: ["Biography", "Drama", "Romance"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "2h 15min"
+        },
+        {
+            title: "The Social Network",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "2h"
+        },
+        {
+            title: "The Pursuit of Happyness",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "Netflix",
+            pg: "PG-13",
+            duration: "1h 57min"
+        },
+        {
+            title: "Selma",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "Hulu",
+            pg: "PG-13",
+            duration: "2h 8min"
+        },
+        {
+            title: "Raging Bull",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 9min"
+        },
+        {
+            title: "The Aviator",
+            categories: ["Biography", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "PG-13",
+            duration: "2h 50min"
+        },
+        {
+            title: "The Conjuring",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "1h 52min"
+        },
+        {
+            title: "Hereditary",
+            categories: ["Horror", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 7min"
+        },
+        {
+            title: "Get Out",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "Netflix",
+            pg: "R",
+            duration: "1h 44min"
+        },
+        {
+            title: "A Quiet Place",
+            categories: ["Horror", "Thriller", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "PG-13",
+            duration: "1h 30min"
+        },
+        {
+            title: "It",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 15min"
+        },
+        {
+            title: "The Shining",
+            categories: ["Horror", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "2h 26min"
+        },
+        {
+            title: "The Exorcist",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "2h 2min"
+        },
+        {
+            title: "Midsommar",
+            categories: ["Horror", "Drama"],
+            streamingPlatform: "Hulu",
+            pg: "R",
+            duration: "2h 28min"
+        },
+        {
+            title: "Insidious",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "Netflix",
+            pg: "PG-13",
+            duration: "1h 43min"
+        },
+        {
+            title: "The Babadook",
+            categories: ["Horror", "Drama"],
+            streamingPlatform: "Prime Video",
+            pg: "NR",
+            duration: "1h 34min"
+        },
+        {
+            title: "The Witch",
+            categories: ["Horror", "Drama"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "1h 32min"
+        },
+        {
+            title: "Scream",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "Paramount+",
+            pg: "R",
+            duration: "1h 51min"
+        },
+        {
+            title: "The Ring",
+            categories: ["Horror"],
+            streamingPlatform: "Hulu",
+            pg: "PG-13",
+            duration: "1h 55min"
+        },
+        {
+            title: "Us",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "HBO Max",
+            pg: "R",
+            duration: "1h 56min"
+        },
+        {
+            title: "Sinister",
+            categories: ["Horror", "Thriller"],
+            streamingPlatform: "Prime Video",
+            pg: "R",
+            duration: "1h 50min"
         }
+        
+        
+        
+        
         
 ];
 
